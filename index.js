099b4c8353414e8d51f5dbb917fd3a104554c587
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-04-26 11:54:21 +0100

    js intro

---
+++ index.js
@@ -0,0 +1,28 @@
+const pi = 3.142;
+let area = 45;
+var breadth = 24;
+
+// alphabet
+// $
+// _
+
+// Identifier Naming Concept
+// - Camel Casing
+let mySchool = "LAUTECH";
+let numberInStock = 45;
+
+// - Pascal Notation
+let MySchool = "OAU";
+let NumberInStock = 209;
+
+let my_school = "UI";
+let number_in_stock = 21;
+
+// We cannot declare a variable twice
+my_school = 120;
+
+area = 120;
+
+console.log(pi);
+console.log(area);
+console.log(breadth);
